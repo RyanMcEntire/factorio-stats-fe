4d8bdf4a93a0b44a6a0055a44a7c6952cc411636
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2024-10-12 11:57:40 -0600

    modify types

--- src/types.ts
+++ src/types.ts
@@ -36,26 +36,20 @@ export interface IApiData {
 }
 
 export interface ApiItem {
-  id: number;
   tick: string;
   item: string;
   amount: number;
-  created_at: string;
 }
 
 export interface ResearchItem {
-  id: number;
   tick: string;
   technology: string;
-  created_at: string;
 }
 
 export interface ModItem {
-  id: number;
   tick: string;
   name: string;
   version: string;
-  created_at: string;
 }
 
 export interface ApiData {
